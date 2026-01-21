802e22514a065216c7f3b919c5f1a3ad41db3486
Guilherme Santos <santosgui678@gmail.com>
2026-01-21 15:03:07 -0300

    feat: getting session info

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,50 +1,81 @@
 'use client';
 
+import { useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
-import { Clock, Home, Users } from "lucide-react"
+import { Clock, Home, Play, Users } from "lucide-react"
+import { toast } from "sonner";
+
+interface Session {
+    id: string;
+    code: string;
+    createdAt: string;
+    startedAt?: string;
+    endedAt?: string;
+    players: [];
+    quiz: Quiz;
+    quizId: string;
+    status: string;
+    updatedAt: string;
+}
+
+interface Quiz {
+    id: string;
+    title: string;
+    description: string;
+    numberOfQuestions: number;
+}
 
 export default function PlayQuiz() {
     const searchParams = useSearchParams()
     const router = useRouter()
 
-    const quiz = {
-        title: "Quiz Teste",
-        description: "Questionário teste"
-    }
-
-    const session = {
-        joinCode: "123456"
-    }
-
-    const players = [
-        { id: "1", name: "Maria Silva", score: 0 },
-        { id: "2", name: "João Santos", score: 0 },
-        { id: "3", name: "Ana Oliveira", score: 0 },
-        { id: "4", name: "Pedro Costa", score: 0 },
-        { id: "5", name: "Lucas Ferreira", score: 0 },
-        { id: "6", name: "Maria Silva", score: 0 },
-        { id: "7", name: "João Santos", score: 0 },
-        { id: "8", name: "Ana Oliveira", score: 0 },
-        { id: "9", name: "Pedro Costa", score: 0 },
-        { id: "10", name: "Lucas Ferreira", score: 0 },
-    ]
-
     const quizId = searchParams.get("quizId");
     const sessionId = searchParams.get("sessionId");
 
+    const [session, setSession] = useState<Session | null>(null);
+
+    useEffect(() => {
+        const fetchSessionData = async () => {
+            if(!quizId || !sessionId) return;
+
+            try{
+                const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/sessions/${sessionId}/quiz/${quizId}`);
+                
+                if(response.ok){
+                    const data = await response.json();
+                    setSession(data);
+                } else{
+                    toast.error('Dados inválidos')
+                    router.replace('/')
+                }
+            }catch(error){
+                console.error(error);
+            }
+        }
+
+        fetchSessionData()
+    }, [])
+
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center text-white p-4">
+        <>
+        <header className="border-b bg-card">
+          <div className="container flex h-14 items-center p-8">
+            <h1 className="text-xl font-bold">EngageQuiz</h1>
+          </div>
+        </header>
+
+        <div className="flex min-h-screen flex-col items-center justify-start mt-5 text-white p-4">
             <Card className="w-full max-w-3xl bg-white/10 backdrop-blur-sm border-none text-white">
                 <CardContent className="p-8 flex flex-col items-center">
-                    <h1 className="text-3xl font-bold mb-2">{quiz.title}</h1>
-                    <p className="text-white/80 mb-6">{quiz.description}</p>
+                    <h1 className="text-3xl font-bold mb-2">{session?.quiz?.title}</h1>
+                    <p className="text-white/80 mb-6">{session?.quiz?.description}</p>
 
                     <div className="text-center mb-8">
                         <p className="text-xl mb-2">Código de Acesso:</p>
                         <p className="text-5xl font-bold tracking-wider bg-white/20 px-8 py-4 rounded-lg">
-                            {session.joinCode}
+                            {session?.code}
                         </p>
                         <p className="mt-4 text-white/80">Conecte-se pelo seu aplicativo.</p>
                     </div>
@@ -55,9 +86,9 @@ export default function PlayQuiz() {
                                 <Users className="h-5 w-5" />
                                 <span>Jogadores conectados</span>
                             </div>
-                            <span className="font-bold">{players.length}</span>
+                            <span className="font-bold">{session?.players?.length}</span>
                         </div>
-                        <div className="bg-white/20 rounded-lg p-4 max-h-40 overflow-y-auto">
+                        {/* <div className="bg-white/20 rounded-lg p-4 max-h-40 overflow-y-auto">
                             <div className="flex flex-wrap gap-2">
                                 {players.map((p) => (
                                     <span key={p.id} className="bg-white/30 px-3 py-1 rounded-full text-sm">
@@ -65,12 +96,12 @@ export default function PlayQuiz() {
                                     </span>
                                 ))}
                             </div>
-                        </div>
+                        </div> */}
                     </div>
 
                     <div className="flex items-center gap-2 text-sm text-white/70 mb-6">
                         <Clock className="h-4 w-4" />
-                        <span>10 questões</span>
+                        <span>{session?.quiz?.numberOfQuestions} questões</span>
                     </div>
 
                     <div className="flex justify-center gap-4">
@@ -83,11 +114,13 @@ export default function PlayQuiz() {
                             Sair
                         </Button>
                         <Button onClick={() => { }} className="bg-green-500 hover:bg-green-600 text-white px-8">
+                            <Play size={16} />
                             Iniciar Quiz
                         </Button>
                     </div>
                 </CardContent>
             </Card>
         </div>
+        </>
     )
 }
