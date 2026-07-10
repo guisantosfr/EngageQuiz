74b1c67f9946a85f9e39355488e6fcd9eded9886
Guilherme Santos <santosgui678@gmail.com>
2026-07-10 15:15:54 -0300

    feat: register endpoint and dto

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Post('register')
+  async register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
+  }
+
   // TODO: Implement @Post('login')
-  
-  // TODO: Implement @Post('register')
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,8 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async register(registerDto: RegisterDto) {
+    const { name, email, password } = registerDto;
+
+    // Verificar e-mail duplicado
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('O e-mail já está em uso.');
+    }
+
+    try {
+      // Gerar hash da senha
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Criar usuário
+      // Definir role padrão = STUDENT (já garantido pelo schema do Prisma)
+      const user = await this.prisma.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          role: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
+
+      return user;
+    } catch (error) {
+      throw new InternalServerErrorException('Erro ao criar usuário.');
+    }
+  }
+
   // TODO: Implement user validation and login logic
-  
-  // TODO: Implement user registration logic
 }
