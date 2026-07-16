6f49e0cce65b2b96e5ac51c42dcc6f903ddb3dad
Guilherme Santos <santosgui678@gmail.com>
2026-07-16 16:27:48 -0300

    feat: add swagger and testing

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('EngageQuiz API')
+    .setDescription('API de criação e gerenciamento de quizzes')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3001, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
