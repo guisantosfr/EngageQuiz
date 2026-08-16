7a764de940ef4923a69f4271f5829c1c9b0e6d62
Guilherme Santos <santosgui678@gmail.com>
2026-08-16 10:27:30 -0300

    feat: add helmet

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -34,6 +35,8 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
+
+  app.use(helmet());
 
   await app.listen(configService.get<number>('PORT') ?? 3000);
 }
