6265ee82adbdbdeb9b1f367b76957c9c2639c5f1
Guilherme Santos <santosgui678@gmail.com>
2026-08-26 15:08:47 -0300

    fix: add public on healthz

--- backend/src/health/health.controller.ts
+++ backend/src/health/health.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { SkipThrottle } from '@nestjs/throttler';
+import { Public } from '../auth/decorators/public.decorator';
 
 @Controller()
 export class HealthController {
+    @Public()
     @Get('healthz')
     @SkipThrottle()
     check() {
