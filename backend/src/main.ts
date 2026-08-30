30c923b73a06693d031bd28ce49b365776a7893e
Guilherme Santos <santosgui678@gmail.com>
2026-08-30 09:35:50 -0300

    refactor: remove docs on production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,19 +8,23 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('EngageQuiz API')
-    .setDescription('API de criação e gerenciamento de quizzes')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
-
   const configService = new ConfigService();
   const nodeEnv = configService.get<string>('NODE_ENV') || 'development';
   const frontendUrl = configService.get<string>('FRONTEND_URL') || 'http://localhost:3000';
+
+  app.enableShutdownHooks();
+
+  if (nodeEnv !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('EngageQuiz API')
+      .setDescription('API de criação e gerenciamento de quizzes')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+  }
 
   if (nodeEnv === 'production') {
     app.enableCors({
